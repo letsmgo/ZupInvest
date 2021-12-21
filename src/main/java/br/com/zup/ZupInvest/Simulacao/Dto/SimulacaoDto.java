8e27ce5855f030a9cf83f1700e681c24d27568d7
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-21 13:42:49 -0300

    Implementar DTO de entrada

---
+++ src/main/java/br/com/zup/ZupInvest/Simulacao/Dto/SimulacaoDto.java
@@ -0,0 +1,64 @@
+package br.com.zup.ZupInvest.Simulacao.Dto;
+
+import br.com.zup.ZupInvest.Simulacao.Enum.Risco;
+
+public class SimulacaoDto {
+    private String email;
+    private String nome;
+    private String cpf;
+    private double valorInvestimento;
+    private int periodoDeAplicacaoMeses;
+    private Risco risco;
+
+    public SimulacaoDto() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorInvestimento() {
+        return valorInvestimento;
+    }
+
+    public void setValorInvestimento(double valorInvestimento) {
+        this.valorInvestimento = valorInvestimento;
+    }
+
+    public int getPeriodoDeAplicacaoMeses() {
+        return periodoDeAplicacaoMeses;
+    }
+
+    public void setPeriodoDeAplicacaoMeses(int periodoDeAplicacaoMeses) {
+        this.periodoDeAplicacaoMeses = periodoDeAplicacaoMeses;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+
+}
---
+++ src/main/java/br/com/zup/ZupInvest/Simulacao/Enum/Risco.java
@@ -0,0 +1,17 @@
+package br.com.zup.ZupInvest.Simulacao.Enum;
+
+public enum Risco {
+
+    ALTO(0.025), MEDIO(0.015), BAIXO(0.005);
+
+    private double taxaDeRendimento;
+
+    Risco(double valor){
+        taxaDeRendimento = valor;
+    }
+
+    public double getTaxaDeRendimento() {
+        return taxaDeRendimento;
+    }
+
+}
