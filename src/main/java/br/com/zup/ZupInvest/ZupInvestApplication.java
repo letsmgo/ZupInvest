9ef843d746bd10dd526c6d44ce1d574f2008704c
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-21 13:35:08 -0300

    first commit

---
+++ src/main/java/br/com/zup/ZupInvest/ZupInvestApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/ZupInvest/ZupInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
