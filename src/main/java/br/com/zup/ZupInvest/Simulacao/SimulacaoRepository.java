22b4fa938d1176afc43aab8ee030ca5db1a99d85
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-21 14:56:14 -0300

    Implementar a repository

---
+++ src/main/java/br/com/zup/ZupInvest/Simulacao/SimulacaoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.ZupInvest.Simulacao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SimulacaoRepository extends CrudRepository<Simulacao, Integer> {
+}
